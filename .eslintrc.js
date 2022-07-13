b27c923525ed752fb239df3fd9306ce279dbf36d
Mathis Bullinger <mathis.bullinger@kevin.eu>
2022-07-13 11:18:10 +0200

    prettier & eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'prettier',
+    'react-app',
+    'plugin:react/recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    'prefer-const': 'warn',
+    'no-console': 'warn',
+    '@typescript-eslint/no-extra-semi': 'off',
+    'semi-style': ['error', 'first'],
+    'no-debugger': 'warn',
+    'no-constant-condition': ['error', { checkLoops: false }],
+    'require-await': 'off',
+    '@typescript-eslint/require-await': 'warn',
+    'no-duplicate-imports': 'off',
+    '@typescript-eslint/no-duplicate-imports': ['warn'],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react/display-name': 'off',
+    'import/no-anonymous-default-export': 'off',
+  },
+};
